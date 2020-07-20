acd64135c70828d837040d260051f4e93a90034e
Payton Gray <paytongray18@gmail.com>
2020-07-20 18:16:17 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  continents = ["north america","Africa","Antartica"]
+  array.pop()
+end
+
+def pop_with_args(array)
+  continents = ["north america","Africa","Antartica"]
+  array.pop(2)
