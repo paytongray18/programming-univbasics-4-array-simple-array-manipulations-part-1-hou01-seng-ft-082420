678376d68baddd4dc3e2fe1b14a606f523db44c9
Payton Gray <paytongray18@gmail.com>
2020-07-20 18:23:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  continents = ["north america","Africa","Antartica"]
+  array.pop()
+end
+
+def pop_with_args(array)
+  continents = ["north america","Africa","Antartica"]
+  array.pop(2)
+end
+
+def using_shift(array)
+  my_favorite_cities = ["houston","atlanta","Vegas"]
+  array.shift()
+end
+
+def shift_with_args(array)
+   my_favorite_cities = ["houston","atlanta","Vegas"]
+   array.shift(2)
+ end
+ 
